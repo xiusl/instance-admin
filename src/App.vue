c3a10ea4ebc71866b98edd60836364a924cc8c54
xiusl <xiushilin@hotmail.com>
2019-08-19 22:15:37 +0800

    view: add font

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import './styles/font.css';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'PTMono', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: #F0F2FD;
