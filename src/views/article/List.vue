218c1fe1f6fc11e90b3534b5fed218b2f50ce9f2
xiusl <xiushilin@hotmail.com>
2019-08-20 17:34:47 +0800

    art: new

--- src/views/article/List.vue
+++ src/views/article/List.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h3>List</h3>
+    <el-input v-model="url" placeholder="url"></el-input>
+    <el-button @click="add">Add</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'articleList'
+  name: 'articleList',
+  data() {
+    return {
+      url: ''
+    }
+  },
+  methods: {
+    add() {
+      console.log(this.url)
+    }
+  }
 }
 </script>
