9effb8daa132cc7435258d223d202908a60af4b9
士林 <xiushilin@hotmail.com>
2020-05-21 18:05:54 +0800

    view: fix help

--- src/views/setting/help.vue
+++ src/views/setting/help.vue
@@ -1,27 +1,19 @@
 <template>
-  <div class="container">
-    <div class="mask"> 
-    </div>
-    <div class="content" style="display:flex;flex-direction:row;">
-      
-      <div style="display: flex;flex-direction:column;">
-      <div style="display: flex;margin-top:80px;" class="">
+    <el-row :gutter="10" class="container">
+        <el-col :lg="3" :xl="1" :span="8" :offset="6"> 
         <img class="logo" src="//image.sleen.top/logo.png" style="height: 80px;"/>
-        <p style="height:80px;line-height:80px;margin:0;margin-left:20px;font-size:32px;font-weight:bold;color:#fff;">哩嗑客户端</p>
-      </div>
-      <div>
-        <p style="font-size:24px;color:white;margin-top:32px;">一个 "收集&分享" 社区</p>
-      </div>
-      </div>
+        <p style="height:80px;line-height:80px;font-size:32px;font-weight:bold;color:#fff;">哩嗑客户端</p>
+        <p style="font-size:24px;color:white;">内容 "收集&分享" 社区</p>
 
+        </el-col>
+        <el-col  :lg="9" :xl="11" :span="4">
       <img class="preview" src="//image.sleen.top/ppabc.png" />
-    </div>
-
+        </el-col>
 
     <div style="position:absolute;width:100%;bottom:2%;">
       <p style="margin:0px;">Copyright © 2020 Like    联系我：<a href="mailto:help@xiusl.com">help@xiusl.com</a></p>
     </div>
-  </div>
+    </el-row>  
 </template>
 
 <script>
@@ -31,33 +23,18 @@ export default {
 </script>
 
 <style scoped>
+* {
+    margin: 0;
+}
 .container {
   background: url('https://cdn.pixabay.com/photo/2020/05/05/17/52/sky-5134180_1280.jpg');
   width: 100%;
   height: 100%;
-  position: absolute;
   background-repeat: no-repeat;
   background-size: 100% 100%;
 }
-.mask {
-  position:absolute;
-  width: 100%;
-  height: 100%;
-  background: #000;
-  opacity: 0.3;
-}
-.content {
-  position: absolute;
-  left: 30%;
-  top: 12%;
-}
-.content .logo {
-  width: 80px;
-  border-radius: 12px;
-}
-.content .preview {
+.preview {
   width: 240px;
-  margin-left: 100px;
   border-radius:30px;
 }
 </style>
